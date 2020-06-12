b81cf9eeceb5c9975e7a0ca73aa3846c4d979e7e
shogoiwamoto <ap7j6st25@gmail.com>
2020-06-12 19:57:33 +0900

    Initial Commit

---
+++ RxSwiftPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftPractice
+//
+//  Created by 岩本省吾 on 2020/06/12.
+//  Copyright © 2020 岩本省吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
