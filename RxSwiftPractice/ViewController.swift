6fb48cf8adb526f492976388fa9a7415ec986975
shogoiwamoto <ap7j6st25@gmail.com>
2020-06-13 21:00:24 +0900

    Add: 観測対象の宣言(Observalパターンの実装)

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var stateSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var freeTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    //オブジェクトの解放用
+    let disposeBag = DisposeBag()
+    
+    enum State: Int {
+        case useButtones
+        case useTextField
+    }
+    
+    //初期化時の初期値の設定
+    let lastSelectingGreeting:Variable<String> = Variable("こんにちは")
+    
+    @IBOutlet var greetingButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //テキスト入力のイベントを観測対象とする
+        let nameObservable:Observable<String?> = nameTextField.rx.text.asObservable()
+        
+        //自由入力のテキストのイベントを観測対象とする
+        let freeObservable:Observable<String?> = freeTextField.rx.text.asObservable()
+        
         // Do any additional setup after loading the view.
     }
+    
+    
 
+    
+    
 
 }
 
